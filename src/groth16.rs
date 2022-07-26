feecae565c7a3a3f073e9dbec206c4e533c76c28
mschoenebeck <matthias.schoenebeck@gmail.com>
2022-07-26 18:18:57 -0500

    added two useful functions

--- src/groth16.rs
+++ src/groth16.rs
@@ -1,5 +1,7 @@
 
 use std::convert::TryInto;
+use std::num::ParseIntError;
+use std::fmt::Write;
 
 use bellman::groth16;
 use bls12_381::{Bls12, Scalar};
@@ -85,6 +87,21 @@ pub fn bytes_to_hex_str(vec: &Vec<u8>) -> String
     }
 
     format!("{:x}", ByteBuf(&vec))
+}
+
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
+
+pub fn encode_hex(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len() * 2);
+    for &b in bytes {
+        write!(&mut s, "{:02x}", b).unwrap();
+    }
+    s
 }
 
 #[cfg(test)]
